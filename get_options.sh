8e4372984142db1c4d19086f73e7c3ffd4c44b8d
Brad Boyle <ojalaquellueva@gmail.com>
2018-08-01 11:11:04 -0700

    get_options: added appendlog option & log replace action

--- get_options.sh
+++ get_options.sh
@@ -16,11 +16,16 @@
 #		in params file.
 # 	-d 	Database to connect to (no default)
 #	-c	Schema to connect to (no default)
+#	-a	Append to existing logfile (=$glogfile). If not 
+#		provided, default is start new (replace old if exists)
 ###########################################################
 
+# Set defaults
 i="true"						# Interactive mode on by default
 e="true"						# Echo on by default
+appendlog="false"				# Append to existing logfile 
 
+# Get options
 while [ "$1" != "" ]; do
     case $1 in
         -n | --nowarnings )		i="false"
@@ -35,7 +40,15 @@ while [ "$1" != "" ]; do
         -c | --schema )         sch="$2"
         						shift
                                 ;;
+        -a | --appendlog )		appendlog="true" 	# Start new logfile, 
+        											# replace if exists
+        						;;
         * )                     echo "invalid option!"; exit 1
     esac
     shift
 done
+
+# Replace global logfile if defined and appendlog=false
+if [ -z ${glogfile+x} ] && [[ "$appendlog" == "true" ]]; then
+	rm -f $glogfile; touch $glogfile
+fi
