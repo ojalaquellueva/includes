8dcb2151ecf789b0e45e34a9a0bccbf9456e9187
Brad Boyle <ojalaquellueva@gmail.com>
2018-11-06 10:41:46 -0800

    Path changes to startup_master.sh

--- startup_master.sh
+++ startup_master.sh
@@ -4,7 +4,10 @@
 # Purpose: Loads parameters, functions & issues startup messages
 #########################################################################
 
-source "$DIR/includes/get_params.sh"	# Parameters, files and paths
+# Reset includes directory
+#includes_dir="${BASH_SOURCE%/*}"
+
+source "get_params.sh"	# Parameters, files and paths
 source "$DIR/includes/get_functions.sh"	# Load functions file(s)
 source "$DIR/includes/get_options.sh"	# Get & set command line options
 
