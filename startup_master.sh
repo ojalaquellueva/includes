c073ac4a307a52d6f6d4d50a043c9c68b1c26006
Brad Boyle <ojalaquellueva@gmail.com>
2018-11-06 10:52:53 -0800

    Path changes to startup_master.sh

--- startup_master.sh
+++ startup_master.sh
@@ -5,9 +5,12 @@
 #########################################################################
 
 # Reset includes directory
-#includes_dir="${BASH_SOURCE%/*}"
+includes_dir="${BASH_SOURCE%/*}"
 
-source "get_params.sh"	# Parameters, files and paths
-source "$DIR/includes/get_functions.sh"	# Load functions file(s)
-source "$DIR/includes/get_options.sh"	# Get & set command line options
+#source "$DIR/get_params.sh"	# Parameters, files and paths
+#source "$DIR/includes/get_functions.sh"	# Load functions file(s)
+#source "$DIR/includes/get_options.sh"	# Get & set command line options
+source $includes_dir"/get_params.sh"	# Parameters, files and paths
+source $includes_dir"/get_functions.sh"	# Load functions file(s)
+source $includes_dir"/get_options.sh"	# Get & set command line options
 
